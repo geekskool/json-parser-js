f6a96e6f4a55eb924f3d94427a42796e4adea114
Anusanthya <anusanthya.shanthakumar@gmail.com>
2016-03-21 09:52:53 +0530

    Modified based on feedback

--- JSONParser.js
+++ JSONParser.js
@@ -6,12 +6,13 @@ var fs = require('fs');
 var data = fs.readFileSync('tester.js');
 inpStr = data.toString();
 
+var match = /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/;
 
 function spaceParse(inpStr) {
 	var nonwhite =inpStr.search(/\S/);
 	if (nonwhite == -1) {
 		return "";
-	} 
+	}
 	return inpStr.slice(nonwhite);
 }
 
@@ -54,32 +55,32 @@ function nullParse(inpStr) {
 function stringParse(inpStr) {
 	var outpt = {};
 	inpStr = spaceParse(inpStr);
-    if (inpStr[0] == '"') {
-    	inpStr = inpStr.substring(1,inpStr.length);
-    	index_val = inpStr.indexOf('"');
-    	inpStr1 = inpStr.substring(0,index_val);
-    	outpt = {out: inpStr1, rem: inpStr.substring(index_val+1, inpStr.length)};
-    	if (outpt.rem == "") {
-    		return outpt.out;
-    	}
-    	return outpt;
-    }
-    return null;
+	if (inpStr[0] == '"') {
+		inpStr = inpStr.substring(1,inpStr.length);
+		index_val = inpStr.indexOf('"');
+		inpStr1 = inpStr.substring(0,index_val);
+		outpt = {out: inpStr1, rem: inpStr.substring(index_val+1, inpStr.length)};
+		if (outpt.rem == "") {
+			return outpt.out;
+		}
+		return outpt;
+	}
+	return null;
 }
 
 
 function numberParse(inpStr) {
 	var outpt = {};
 	inpStr = spaceParse(inpStr);
-	if (match = /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/.exec(inpStr)) {
+	if (match.exec(inpStr)) {
 		num_len = match[0].length;
-	    var num = parseFloat(match[0]);
-	    var d = inpStr.substring(num_len, inpStr.length);
-	    outpt = {out: num, rem: inpStr.substring(num_len, inpStr.length)};
-	    if (outpt.rem == "") {
-	    	return outpt.out;
-	    }
-	    return outpt;
+		var num = parseFloat(match[0]);
+		var d = inpStr.substring(num_len, inpStr.length);
+		outpt = {out: num, rem: inpStr.substring(num_len, inpStr.length)};
+		if (outpt.rem == "") {
+			return outpt.out;
+		}
+		return outpt;
 	}
 	return null;
 }
@@ -121,25 +122,25 @@ function objectParse(inpStr) {
 		inpStr = inpStr.substring(1,inpStr.length);
 		while (inpStr[0] != "}") {
 			inpStr = spaceParse(inpStr);
+			key = stringParse(inpStr);
+			inpStr = spaceParse(key.rem);
+			if (inpStr[0] == ":") {
+				inpStr = inpStr.slice(1);
+			}
+			value = parseWay(inpStr);
+			inpStr = spaceParse(value.rem);
+			outArrObj[key.out] = value.out;
 			if (inpStr[0] == ",") {
 				inpStr = inpStr.slice(1);
 			}
-          	key = stringParse(inpStr);
- 			inpStr = spaceParse(key.rem);
-            if (inpStr[0] == ":") {
-            	inpStr = inpStr.slice(1);
-            }
-            value = parseWay(inpStr);
-            inpStr = spaceParse(value.rem);
- 			outArrObj[key.out] = value.out;
- 		}
- 		outpt = {out: outArrObj, rem: inpStr.slice(1)};
- 		if (outpt.rem == "") {
- 			return outpt.out;
- 		}
- 		return outpt;
- 	}
- 	return null;
+		}
+		outpt = {out: outArrObj, rem: inpStr.slice(1)};
+		if (outpt.rem == "") {
+			return outpt.out;
+		}
+		return outpt;
+	}
+	return null;
 }
 
 
